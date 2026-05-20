cfe6ce327c20b738aa3a2459d7e272d628e34f76
adikamh <haikaladika272@gmail.com>
2026-05-20 15:15:30 +0700

    Menambahkan untuk login google dan sensor data pengguna

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -20,3 +20,26 @@ if (! function_exists('mask_phone')) {
             . substr($phone, -4);
     }
 }
+
+if (! function_exists('mask_address')) {
+    function mask_address(?string $address): string
+    {
+        $address = trim((string) $address);
+
+        if ($address === '') {
+            return '';
+        }
+
+        $length = strlen($address);
+
+        if ($length <= 12) {
+            return substr($address, 0, 2)
+                . str_repeat('*', max(0, $length - 4))
+                . substr($address, -2);
+        }
+
+        return substr($address, 0, 8)
+            . str_repeat('*', min(24, max(8, $length - 12)))
+            . substr($address, -4);
+    }
+}
